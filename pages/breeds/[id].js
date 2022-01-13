23de24d739e04d10cb9c8f1284eced0f87a3e871
Kariuki George <85384821+smiley-geek@users.noreply.github.com>
2022-01-13 20:12:28 +0300

    Update [id].js

--- pages/breeds/[id].js
+++ pages/breeds/[id].js
@@ -54,7 +54,7 @@ export async function getStaticPaths() {
 
   // We'll pre-render only these paths at build time.
   // { fallback: false } means other routes should 404.
-  return { paths, fallback: true};
+  return { paths, fallback: false};
 }
 
 export async function getStaticProps({ params }) {
